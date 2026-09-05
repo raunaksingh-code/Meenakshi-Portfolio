71b5cdf4e70d783b5399c17c5be659a4e99386f7
QCPL-OPL <nikkisharma2904@gmail.com>
2026-09-06 00:31:56 +0530

    fix: resolve SVG wave transition artifact in Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -106,9 +106,10 @@ export function Hero() {
       </div>
 
       {/* Subtle wave transition to the next section */}
-      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-none z-0 rotate-180">
-        <svg className="relative block w-full h-[50px] md:h-[100px]" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V120H0V95.8C59.71,118,130.85,132,201.33,115.2,243.68,105,283.47,81.4,321.39,56.44Z" className="fill-background"></path>
+      <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0]">
+        <svg className="relative block w-[calc(100%+1.3px)] h-[40px] md:h-[80px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="fill-background opacity-0"></path>
+          <path d="M0,120 C200,80 400,0 600,0 C800,0 1000,80 1200,120 Z" className="fill-background"></path>
         </svg>
       </div>
 
